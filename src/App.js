2cae417cef4fc9558473914869272317e2429fb1
NomadDaniel <reachingdaniel@gmail.com>
2020-04-15 16:32:58 -0400

    MVP, working on stretch for Date using Moments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+import CardBuilder from './Components/CardBuilder'
+import Date from './Components/Date'
 
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
+
+      <CardBuilder/>
+      <Date/>
     </div>
   );
 }
